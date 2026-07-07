7b095bbcb8c09260716a346780e53f5a92373e5a
Ali Bagheri <alirs20016@gmail.com>
2026-07-07 17:31:27 +0330

    Day 8 - Contact Book

---
+++ practice2.py
@@ -0,0 +1,7 @@
+contacts = [
+    {"name": "Ali", "phone": "111"},
+    {"name": "Sara", "phone": "222"},
+    {"name": "Reza", "phone": "333"}
+]
+for contact in contacts:
+    print(contact['name'],'->',contact['phone'])
---
+++ project.py
@@ -0,0 +1,68 @@
+contacts = [
+    {'name': 'Ali', "phone": "111"},
+    {'name': 'Sara', "phone": "222"},
+    {'name': 'Reza', "phone": "333"}
+]
+
+def show_menu():
+    menu1=int(input("1_add contact\n2_Show Contacts\n3_Search by Name\n4_Delete Contact\n5_exit\n"))
+    return menu1
+
+def add_contact(name,number):
+    contact={
+        'name':name,
+        'phone':number
+    }
+    contacts.append(contact)
+    print("It was successful.")
+
+def input_name():
+    enter_name=input('enter contact name: ')
+    enter_name=enter_name.lower()
+    return enter_name.title()
+
+def input_phone():
+    enter_phone=input('enter number: ')
+    return enter_phone
+
+def print_contact():
+    for contact  in contacts:
+        print('name:',contact['name'],'\nphone:',contact['phone'],'\n-------------')
+        
+        
+def search(name):
+    for contact in contacts:
+        if name == contact['name']:
+           print('found')
+           return(contact['phone'])
+    return('not found')
+
+def delete(name):
+    for contact in contacts:
+        if name == contact['name']:
+            contacts.remove(contact)  
+            return("It was successful.")
+                   
+    return('not found')
+        
+
+while True:
+    menu=show_menu()
+    if menu==1:
+        name=input_name()
+        number=input_phone()
+        add_contact(name,number)
+    elif menu==2:
+        print_contact()
+    elif menu==3:
+        name=input_name()
+        print(search(name))
+
+    elif menu==4:
+        name=input_name()
+        print(delete(name))
+    elif menu==5:
+        print('goodbye')    
+        break
+        
+
